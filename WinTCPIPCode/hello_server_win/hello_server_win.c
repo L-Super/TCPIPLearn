fea32f6511c76cdd9c70e3c1d491c74db455afb3
LSuper <120311070@qq.com>
2022-02-02 17:50:46 +0800

    vault backup: 2022-02-02 17:50:46

--- WinTCPIPCode/hello_server_win/hello_server_win.c
+++ WinTCPIPCode/hello_server_win/hello_server_win.c
@@ -3,7 +3,7 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
-#include <WinSock2.h>
+#include <winsock2.h>
 #pragma comment(lib, "Ws2_32.lib")
 
 int main(int argc, char* argv[])
@@ -18,10 +18,12 @@ int main(int argc, char* argv[])
 		printf("Usage: %s <port>\n",argv[0]);
 		exit(1);
 	}
+	// 初始化，设置程序用到的Winsock版本，并初始化相应版本的库
 	if(WSAStartup(MAKEWORD(2,2),&wsaData) != 0)
 	{
 		printf("WSAStartup() error!");
 	}
+	// 返回套接字句柄
 	hServerSock = socket(PF_INET,SOCK_STREAM,0);
 	if(hServerSock == INVALID_SOCKET)
 	{
@@ -37,14 +39,17 @@ int main(int argc, char* argv[])
 		printf("bind() error");
 	if(listen(hServerSock, 5) == SOCKET_ERROR)
 		printf("listen() error");
+
 	szClientAddr = sizeof(clientAddr);
 	hClientSock = accept(hServerSock,(SOCKADDR*)&clientAddr,&szClientAddr);
 	if(hClientSock == INVALID_SOCKET)
 		printf("accept() error");
 	send(hClientSock,message,sizeof(message),0);
 
+	// 关闭套接字
 	closesocket(hClientSock);
 	closesocket(hServerSock);
+	// 注销库
 	WSACleanup();
 	return 0;
 }
